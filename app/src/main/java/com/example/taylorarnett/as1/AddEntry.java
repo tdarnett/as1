aa5c739e9958c62e2e674b72f234e473a5f4ff89
tdarnett <tarnett1@mac.com>
2016-01-22 10:39:31 -0700

    added saveFields in AddEntry.java file.

--- app/src/main/java/com/example/taylorarnett/as1/AddEntry.java
+++ app/src/main/java/com/example/taylorarnett/as1/AddEntry.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 
 public class AddEntry extends ActionBarActivity {
@@ -15,6 +17,16 @@ public class AddEntry extends ActionBarActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
        // RelativeLayout layout = (RelativeLayout) findViewById(R.id.content);
         //Intent intent = getIntent();
+    }
+
+    public void saveFields (View view) {
+        EditText edit_date = (EditText) findViewById(R.id.date_text);
+        EditText edit_station = (EditText) findViewById(R.id.station_text);
+        EditText edit_odometer = (EditText) findViewById(R.id.odometer_text);
+        EditText edit_fuelGrade = (EditText) findViewById(R.id.fuelGrade_text);
+        EditText edit_fuelAmount = (EditText) findViewById(R.id.fuelAmount_text);
+        EditText edit_fuelUnitCost = (EditText) findViewById(R.id.fuelUnitCost_text);
+        
     }
 
 }
